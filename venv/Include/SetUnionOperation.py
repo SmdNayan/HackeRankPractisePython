3b29b8997b389f46571f8d48d340f5678073bd76
SmdNayan <smd.nayan69@gmail.com>
2018-10-06 00:31:13 +0600

    Sort the program of NoIdea and add three new Solution

---
+++ venv/Include/SetUnionOperation.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    n = int(input())
+    sr1 = set(map(int, input().split(' ')))
+    nc = int(input())
+    sr2 = set(map(int, input().split(' ')))
+
+    u = sr1.union(sr2)
+
+    print(len(u))
